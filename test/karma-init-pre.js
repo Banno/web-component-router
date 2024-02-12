d568b7c70578e02c78d2e75a7677dd3b16901926
John Robinson <jrobinson@jackhenry.com>
2024-02-12 14:26:58 -0500

    tests: fix path-to-regexp path in karma import map

--- test/karma-init-pre.js
+++ test/karma-init-pre.js
@@ -2,6 +2,6 @@ document.write(`<script type="importmap">{
   "imports": {
     "@webcomponents/": "/base/node_modules/@webcomponents/",
     "@polymer/": "/base/node_modules/@polymer/",
-    "path-to-regexp/dist.es2015/index.js": "/base/node_modules/path-to-regexp/dist.es2015/index.js"
+    "path-to-regexp": "/base/node_modules/path-to-regexp/dist.es2015/index.js"
   }
 }\x3c/script>`);
